b938253e3674dd9f815d2c432faffeed17745229
JuanSheba <jsoto@redborder.com>
2023-11-27 08:52:24 +0000

    first version

---
+++ spec/modules/monitor/cluster_monitor_spec.rb
@@ -0,0 +1,58 @@
+require "spec_helper"
+describe service("redborder-monitor") do
+  it { should be_running }
+end
+
+describe file("/etc/redborder-monitor/config.json") do
+  it { should exist }
+  # Puedes añadir más verificaciones para el archivo de configuración aquí
+end
+
+describe service("kafka") do
+  it { should be_running }
+end
+
+describe service("logstash") do
+  it { should be_running }
+end
+
+describe command("rb_get_topics | grep monitor") do
+  its(:stdout) { should match /rb_monitor/ }
+  its(:stdout) { should match /rb_monitor_post/ }
+  # Añade aquí más matches para otros topics específicos que necesites verificar
+end
+
+describe file("/etc/logstash/pipelines/monitor/00_input.conf") do
+  it { should exist }
+end
+
+describe file("/etc/logstash/pipelines/monitor/01_monitor.conf") do
+  it { should exist }
+end
+
+describe file("/etc/logstash/pipelines/monitor/99_output.conf") do
+  it { should exist }
+end
+
+describe file("/etc/logstash/pipelines.yml") do
+  it { should contain "pipeline.id: monitor-pipeline" }
+  it { should contain 'path.config: "/etc/logstash/pipelines/monitor"' }
+end
+
+describe file("/etc/druid/realtime/rb_realtime.spec") do
+  it { should exist }
+  # Añade aquí verificaciones específicas para el contenido del archivo
+end
+
+describe command("ls /tmp/realtime/rb_monitor") do
+  its(:exit_status) { should eq 0 }
+end
+
+# Nota: Ejecutar estos comandos puede ser más complejo y depende del entorno
+describe command("timeout 3 rb_consumer.sh -t rb_monitor_post") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("rb_consumer.sh -t rb_monitor_post_<namespace>") do
+  its(:exit_status) { should eq 0 }
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,22 @@
-require 'serverspec'
-require 'net/ssh'
-require 'tempfile'
-require 'highline/import'
+require "serverspec"
+require "net/ssh"
+require "tempfile"
+require "highline/import"
 
 set :backend, :ssh
 set :disable_sudo, true
-set :path, '/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin'
+set :path, "/usr/lib/redborder/bin:/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin"
 
 # ssh setup
-host = ENV['TARGET_HOST']
+host = ENV["TARGET_HOST"]
 options = Net::SSH::Config.for(host)
 set :host, options[:host_name] || host
-options[:user] ||= ENV['LOGIN_USERNAME'] || 'root'
+options[:user] ||= ENV["LOGIN_USERNAME"] || "root"
 
-if ENV['ASK_LOGIN_PASSWORD']
-    options[:password] = ask("\nEnter login password: ") { |q| q.echo = false }
+if ENV["ASK_LOGIN_PASSWORD"]
+  options[:password] = ask("\nEnter login password: ") { |q| q.echo = false }
 else
-    options[:password] = ENV['LOGIN_PASSWORD'] || 'redborder'
+  options[:password] = ENV["LOGIN_PASSWORD"] || "redborder"
 end
-  
+
 set :ssh_options, options
-
-
-
-
