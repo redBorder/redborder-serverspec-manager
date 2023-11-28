98a667fc9383299abbfffed0c5f8b96eb05d1d41
Miguel Negron <manegron@redborder.com>
2023-11-28 18:26:14 +0000

    Fix linter

--- spec/services/snmpd_spec.rb
+++ spec/services/snmpd_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
-set :os, :family => 'redhat', :release => '9', :arch => 'x86_64'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 describe 'Checking net-snmp' do
   describe package('net-snmp') do
