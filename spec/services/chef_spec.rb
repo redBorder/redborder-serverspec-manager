3fd9857c503507646e4d0457de762963a8e785a2
vimesa <vimesa@redborder.com>
2023-11-29 14:23:54 +0000

    Registered in consul

--- spec/services/chef_spec.rb
+++ spec/services/chef_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'json'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-describe 'Checking chef' do
-  describe package('redborder-chef-server') do
-    it { should be_installed }
+packages = %w[
+  redborder-chef-server cookbook-chef-server
+]
+describe 'Checking chef...' do
+  packages.each do |package|
+    describe package(package) do
+      it { should be_installed }
+    end
   end
 
   describe service('chef-client') do
@@ -16,5 +22,22 @@ describe 'Checking chef' do
   describe file('/etc/chef/client.rb') do
     it { should exist }
     it { should be_file }
+  end
+
+  describe port(4443) do
+    it { should be_listening }
+  end
+
+  describe 'Registered in consul' do
+    service_name = 'erchef'
+    command = "curl -s http://localhost:8500/v1/catalog/service/#{service_name} | jq -r '.[].Address'"
+    service_health = "curl -s http://localhost:8500/v1/health/service/#{service_name}"
+    ips = command(command).stdout.split("\n")
+    it 'Should be registered and enabled' do
+      # expect(ips).not_to be_empty
+      enabled = JSON.parse(service_health)[0]['Checks'][0]['Status'] == 'passing'
+      expect(enabled).to be true
+      puts "Output: #{ips}" if enabled
+    end
   end
 end
