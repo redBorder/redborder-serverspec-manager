539d06b46c732fc4053b39e88ceefcaefd14e9d7
vimesa <vimesa@redborder.com>
2023-11-29 07:49:39 +0000

    Rubocop

--- spec/configuration/network_spec.rb
+++ spec/configuration/network_spec.rb
@@ -10,13 +10,13 @@ describe 'All interfaces: ' do
   interfaces.each do |interface|
     describe interface(interface) do
       it { should exist }
-      #it { should be_up }
+      # it { should be_up }
     end
   end
 end
 
 # Management network
-puts "Configuration"
+puts 'Configuration'
 
 describe 'Management network' do
   let(:ip_address) { '10.1.209.' }
@@ -27,12 +27,12 @@ end
 
 # Sync network
 
-describe 'Sync network' do 
+describe 'Sync network' do
   sync = command('ip addr show').stdout
-  it 'Have a network sync' do 
+  it 'Have a network sync' do
     interfaces_with_ip = sync.scan(/inet\s+(\d+\.\d+\.\d+\.\d+)/).flatten
     filtered_ips = interfaces_with_ip.select { |ip| ip.start_with?('10.1.') }
-    expect(filtered_ips.length).to be >= 2    
+    expect(filtered_ips.length).to be >= 2
   end
 end
 
@@ -41,7 +41,7 @@ end
 describe 'DNS' do
   resolv_content = command('cat /etc/resolv.conf').stdout
   it 'It should have a nameserver' do
-    expect(resolv_content).to match /nameserver/
+    expect(resolv_content).to match(/nameserver/)
   end
 end
 
@@ -49,8 +49,8 @@ end
 
 describe 'Password' do
   user_root = command('sudo passwd -S root').stdout
-  it 'User root have a password'do
-    expect(user_root).to include('P')  
+  it 'User root have a password' do
+    expect(user_root).to include('P')
   end
 end
 
@@ -58,10 +58,10 @@ end
 
 current_time = Time.now.utc
 
-describe 'Date time'do
+describe 'Date time' do
   date_time = command('date -u "+%a %b %e %H:%M %Z %Y"').stdout
   it 'Current date and time in utc'do
-    expect(date_time).to include("#{current_time.strftime("%a %b %e %H:%M %Z %Y")}")
+  expect(date_time).to include(current_time.strftime('%a %b %e %H:%M %Z %Y').to_s)
     puts "OUTPUT: #{date_time}"
   end
 end
@@ -71,17 +71,17 @@ end
 describe 'Hostname' do
   hostname = command('hostname').stdout
   it 'Hostname is set' do
-    expect(hostname).not_to be_empty 
+    expect(hostname).not_to be_empty
     puts "OUTPUT: #{hostname}"
   end
 end
 
 # IPMI
 
-describe 'IPMI' do 
+describe 'IPMI' do
   ipmi_status = command('rpm -qa | grep ipmi').stdout
   it 'Status IPMI' do
-    expect(ipmi_status).to include("ipmi")
+    expect(ipmi_status).to include('ipmi')
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ options = Net::SSH::Config.for(host)
 set :host, options[:host_name] || host
 options[:user] ||= ENV['LOGIN_USERNAME'] || 'root'
 
-
 options[:password] = if ENV['ASK_LOGIN_PASSWORD']
                        ask("\nEnter login password: ") { |q| q.echo = false }
                      else
