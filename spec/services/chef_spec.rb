f1fd88ef399d3851c3cfafe04c8056997b226803
vimesa <vimesa@redborder.com>
2023-11-29 15:08:03 +0000

    Modify registered in consul

--- spec/services/chef_spec.rb
+++ spec/services/chef_spec.rb
@@ -28,16 +28,16 @@ describe 'Checking chef...' do
     it { should be_listening }
   end
 
-  describe 'Registered in consul' do
+  describe 'Registered in consul and enabled' do
     service_name = 'erchef'
-    command = "curl -s http://localhost:8500/v1/catalog/service/#{service_name} | jq -r '.[].Address'"
-    service_health = "curl -s http://localhost:8500/v1/health/service/#{service_name}"
-    ips = command(command).stdout.split("\n")
+    response = "curl -s http://localhost:8500/v1/catalog/service/#{service_name} | jq -r '.[].Address'"
+    health = "curl -s http://localhost:8500/v1/health/service/#{service_name} | jq -r '.[].Checks' | jq -r '.[].Status'"
+    service_health = command(health).stdout.split("\n")
+    ips = command(response).stdout.split("\n")
     it 'Should be registered and enabled' do
-      # expect(ips).not_to be_empty
-      enabled = JSON.parse(service_health)[0]['Checks'][0]['Status'] == 'passing'
-      expect(enabled).to be true
-      puts "Output: #{ips}" if enabled
+      expect(ips).not_to be_empty
+      passing_checks = service_health.to_s.chomp
+      expect(passing_checks).to include('passing')
     end
   end
 end
