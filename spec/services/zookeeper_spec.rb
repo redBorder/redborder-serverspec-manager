c6e1bbcc2eb1a59df37b576ea62827f6cd243f65
Juan Pablo <jcabrillana@redborder.com>
2023-12-01 11:51:52 +0000

    The code was refactored

--- spec/services/zookeeper_spec.rb
+++ spec/services/zookeeper_spec.rb
@@ -8,76 +8,57 @@ packages = %w[
 ]
 
 service = 'zookeeper'
+port = 2181
 
-def describe_package(package)
-  describe package(package) do
-    before do
-      skip("#{package} is not installed, skipping...") unless package(package).installed?
-    end
-
-    it 'is expected to be installed' do
-      expect(package(package).installed?).to be true
-    end
-  end
-end
-
-def describe_service(service)
-  service_status = command("systemctl is-enabled #{service}").stdout.strip
-
-  if service_status == 'enabled'
-    describe_running_service(service)
-  else
-    describe_disabled_service(service)
-  end
-end
-
-def describe_running_service(service)
-  describe service(service) do
-    it { should be_enabled }
-    it { should be_running }
-  end
-
-  describe_port(2181)
-
-  describe_consul_registration(service)
-end
-
-def describe_disabled_service(service)
-  describe service(service) do
-    it { should_not be_enabled }
-    it { should_not be_running }
-  end
-
-  describe_port(2181)
-end
-
-def describe_port(port)
-  describe port(port) do
-    it { should be_listening }
-  end
-end
-
-def describe_consul_registration(service)
-  api_endpoint = 'http://localhost:8500/v1'
-  service_json = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -r '.[]'").stdout
-  health = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'").stdout.strip
-  registered = JSON.parse(service_json).key?('Address') && health == 'passing' ? true : false
-
-  describe 'Registered in consul' do
-    it 'Should be registered and enabled' do
-      expect(registered).to be true
-    end
-  end
-end
-
-describe "Checking #{service}..." do
+describe "Checking packages for #{service}..." do
   packages.each do |package|
-    describe_package(package)
+    describe package(package) do
+      before do
+        skip("#{package} is not installed, skipping...") unless package(package).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
+    end
   end
+end
 
-  describe_service(service)
+service_status = command("systemctl is-enabled #{service}").stdout
+service_status = service_status.strip
 
-  describe_port(2181)
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
 
-  describe_consul_registration(service)
+    describe port(port) do
+      it { should be_listening }
+    end
+
+    describe 'Registered in consul' do
+      api_endpoint = 'http://localhost:8500/v1'
+      service_json = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -r '.[]'").stdout
+      health = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'").stdout
+      health = health.strip
+      registered = JSON.parse(service_json).key?('Address') && health == 'passing' ? true : false
+      it 'Should be registered and enabled' do
+        expect(registered).to be true
+      end
+    end
+  end
+end
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+
+    describe port(port) do
+      it { should_not be_listening }
+    end
+  end
 end
