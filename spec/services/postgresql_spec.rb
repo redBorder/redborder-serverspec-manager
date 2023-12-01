5ce9756b102564f585e33d043692e25ac8178944
JuanSheba <jsoto@redborder.com>
2023-12-01 13:25:43 +0000

    Refine PostgreSQL Serverspec tests with dynamic service checks

--- spec/services/postgresql_spec.rb
+++ spec/services/postgresql_spec.rb
@@ -1,46 +1,61 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-databases = %w[bifrost druid monitors oc_id opscode_chef postgres radius redborder template0
-               template1]
+service = 'postgresql'
+port = 5432
+databases = %w[bifrost druid monitors oc_id opscode_chef postgres radius redborder template0 template1]
 
-# PostgreSQL Tests
-describe 'PostgreSQL Tests' do
-  # Test if PostgreSQL package is installed
-  describe package('postgresql') do
+describe 'Checking PostgreSQL Package...' do
+  describe package(service) do
     it { should be_installed }
-  end
-
-  # Test if the PostgreSQL service is enabled and running
-  describe service('postgresql') do
-    it { should be_enabled }
-    it { should be_running }
-  end
-
-  # Test if PostgreSQL is listening on the default port (5432)
-  describe port(5432) do
-    it { should be_listening }
-  end
-
-  # Test database connection
-  describe command('psql -U redborder -d redborder -h localhost -c "\\l"') do
-    databases.each do |db|
-      its('stdout') { should match(/#{db}/) }
-    end
-    its('exit_status') { should eq 0 }
   end
 end
 
-# PostgreSQL Configuration File Tests
+service_status = command("systemctl is-enabled #{service}").stdout.strip
+
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(port) do
+      it { should be_listening }
+    end
+  end
+
+  describe 'Database Connection' do
+    describe command('psql -U redborder -d redborder -h localhost -c "\\l"') do
+      databases.each do |db|
+        its('stdout') { should match(/#{db}/) }
+      end
+      its('exit_status') { should eq 0 }
+    end
+  end
+end
+
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+
+    describe port(port) do
+      it { should_not be_listening }
+    end
+  end
+end
+
 describe 'PostgreSQL Configuration Files' do
-  # Check if postgresql.conf exists and has correct permissions
   describe file('/var/lib/pgsql/data/postgresql.conf') do
     it { should exist }
     it { should be_owned_by 'postgres' }
   end
 
-  # Check if pg_hba.conf exists and has correct permissions
   describe file('/var/lib/pgsql/data/pg_hba.conf') do
     it { should exist }
     it { should be_owned_by 'postgres' }
