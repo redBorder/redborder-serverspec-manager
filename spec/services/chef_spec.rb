c75a3a6082ca4cd49f7141c8adc3b37350bd02b7
Vimesa <vimesa@localhost.localdomain>
2023-12-01 12:36:53 +0000

    Modify chef_spec.rb, add systemctl is-enabled

--- spec/services/chef_spec.rb
+++ spec/services/chef_spec.rb
@@ -7,34 +7,70 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 packages = %w[
   redborder-chef-server cookbook-chef-server
 ]
-describe 'Checking chef...' do
+
+service = 'chef-client'
+config_file = '/etc/chef/client.rb'
+port = 4443 
+
+describe "Checking packages for #{service}..." do
   packages.each do |package|
     describe package(package) do
-      it { should be_installed }
+      before do
+        skip("#{package} is not installed, skipping...") unless package(package).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
     end
   end
+end
 
-  describe service('chef-client') do
-    it { should be_enabled }
-    it { should be_running }
+service_status = command("systemctl is-enabled #{service}").stdout
+service_status = service_status.strip
+
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe file(config_file) do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe port(port) do
+      it { should be_listening }
+    end
+
+    describe 'Registered in consul' do
+      api_endpoint = 'http://localhost:8500/v1'
+      service_json = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -r '.[]'").stdout
+      health = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'").stdout
+      health = health.strip
+      registered = JSON.parse(service_json).key?('Address') && health == 'passing' ? true : false
+      it 'Should be registered and enabled' do
+        expect(registered).to be true
+      end
+    end
   end
+end
 
-  describe file('/etc/chef/client.rb') do
-    it { should exist }
-    it { should be_file }
-  end
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
 
-  describe port(4443) do
-    it { should be_listening }
-  end
+    describe file(config_file) do
+      it { should_not exist }
+    end
 
-  describe 'Registered in consul' do
-    service_json = command("curl -s http://localhost:8500/v1/catalog/service/erchef | jq -r '.[]'").stdout
-    health = command("curl -s http://localhost:8500/v1/health/service/erchef | jq -r '.[].Checks[0].Status'").stdout
-    health = health.strip
-    registered = JSON.parse(service_json).key?('Address') && health == 'passing' ? true : false
-    it do
-      expect(registered).to be true
+    describe port(port) do
+      it { should_not be_listening }
     end
   end
 end
