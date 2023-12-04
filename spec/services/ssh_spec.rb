0009cecb06b097914561605ca7d1a674a843e786
Luis Blanco <ljblanco@redborder.com>
2023-12-04 15:15:43 +0000

    Main host can reach every node

--- spec/services/ssh_spec.rb
+++ spec/services/ssh_spec.rb
@@ -14,3 +14,16 @@ describe 'Cluster Nodes' do
     end
   end
 end
+
+describe 'Reachable nodes' do
+  describe command('serf members | awk \'{split($2, a, ":"); print a[1]}\'') do
+    its(:exit_status) { should eq 0 }
+
+    it 'Main host can reach every node' do
+      target_ips = subject.stdout.chomp.split("\n")
+      target_ips.each do |target_ip|
+        expect(host(target_ip)).to be_reachable.with(:port => 22) # Cambia el puerto según el servicio que quieras comprobar
+      end
+    end
+  end
+end
