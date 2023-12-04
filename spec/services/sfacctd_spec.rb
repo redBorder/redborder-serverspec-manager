217dbf0d0f1a42e196a85022caf046807ea4e56a
nverschaeve <nverschaeve@redborder.com>
2023-12-04 08:15:48 +0000

    upgrade sfacctd

--- spec/services/sfacctd_spec.rb
+++ spec/services/sfacctd_spec.rb
@@ -3,15 +3,54 @@
 require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-describe file('/etc/pmacct/sfacctd.conf') do
-  it { should exist }
+service = 'sfacctd'
+port = 6343
+service_status = command("systemctl is-enabled #{service}").stdout.strip
+
+packages = %w[cookbook-pmacct pmacct]
+
+describe "Checking packages for #{service}..." do
+  packages.each do |package|
+    describe package(package) do
+      before do
+        skip("#{package} is not installed, skipping...") unless package(package).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
+    end
+  end
 end
 
-describe service('sfacctd') do
-  it { should be_enabled }
-  it { should be_running }
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(port) do
+      it { should be_listening }
+    end
+  end
 end
 
-describe port(6343) do
-  it { should be_listening }
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+
+    describe port(port) do
+      it { should_not be_listening }
+    end
+  end
+end
+
+describe "Checking #{service} for config file" do
+  describe file("/etc/pmacct/#{service}.conf") do
+    it { should exist }
+  end
 end
