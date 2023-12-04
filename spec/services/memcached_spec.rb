180935ccd4d8ab44d5562568c9734b1ecdcedac8
JuanSheba <jsoto@redborder.com>
2023-12-04 13:01:38 +0000

    Refactor Mencached Serverspec tests to check if the service is enabled

--- spec/services/memcached_spec.rb
+++ spec/services/memcached_spec.rb
@@ -3,45 +3,64 @@
 require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-describe 'Checking Memcached Service - Basic Checks' do
-  # Verifies the installation of the Memcached package.
-  describe package('memcached') do
-    it { should be_installed }
+service = 'memcached'
+service_status = command("systemctl is-enabled #{service}").stdout.strip
+
+if service_status == 'enabled'
+  describe 'Checking Memcached Service - Basic Checks' do
+    # Verifies the installation of the Memcached package.
+    describe package(service) do
+      it { should be_installed }
+    end
+
+    # Checks the status of the Memcached service.
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    # Verifies the basic configuration of Memcached.
+    describe 'Configuration' do
+      describe file('/usr/lib/sysusers.d/memcached.conf') do
+        it { should exist }
+      end
+    end
   end
 
-  # Checks the status of the Memcached service.
-  describe service('memcached') do
-    it { should be_enabled }
-    it { should be_running }
-  end
+  describe 'Checking Memcached Service - Advanced Checks' do
+    # Checks network and connectivity aspects.
+    describe 'Network and Connectivity' do
+      describe port(11_211) do
+        it { should be_listening }
+      end
+    end
 
-  # Verifies the basic configuration of Memcached.
-  describe 'Configuration' do
-    describe file('/usr/lib/sysusers.d/memcached.conf') do
-      it { should exist }
+    # Verifies the Memcached logs.
+    describe 'Memcached Logs' do
+      describe file('/var/log/memcached.log') do
+        its(:content) { should_not match(/error/i) }
+      end
+    end
+
+    # Verifies the user running the Memcached process.
+    describe 'Process User' do
+      describe command("ps aux | grep [m]emcached | awk '{print $1}'") do
+        its(:stdout) { should match(/memcach\+/) }
+      end
     end
   end
 end
 
-describe 'Checking Memcached Service - Advanced Checks' do
-  # Checks network and connectivity aspects.
-  describe 'Network and Connectivity' do
-    describe port(11_211) do
-      it { should be_listening }
-    end
-  end
+if service_status == 'disabled'
+  describe 'Memcached Service Checks for Disabled Service' do
+    describe service(service) do
+      it 'should not be enabled' do
+        expect(subject).not_to be_enabled
+      end
 
-  # Verifies the Memcached logs.
-  describe 'Memcached Logs' do
-    describe file('/var/log/memcached.log') do
-      its(:content) { should_not match(/error/i) }
-    end
-  end
-
-  # Verifies the user running the Memcached process.
-  describe 'Process User' do
-    describe command("ps aux | grep [m]emcached | awk '{print $1}'") do
-      its(:stdout) { should match(/memcach\+/) }
+      it 'should not be running' do
+        expect(subject).not_to be_running
+      end
     end
   end
 end
