e03b459516fc69ec79684f1746ffb2303b811fbb
Luis Blanco <ljblanco@redborder.com>
2023-12-04 14:31:57 +0000

    check if at least 3 nodes are alive

--- spec/services/ssh_spec.rb
+++ spec/services/ssh_spec.rb
@@ -5,3 +5,12 @@ require 'spec_helper'
 describe port(22) do
   it { should be_listening }
 end
+
+describe 'Cluster Nodes' do
+  describe command('serf members | grep -c "alive"') do 
+    it 'There are at least 3 alive nodes' do
+      n_nodes = subject.stdout.to_i
+      expect(n_nodes).to be > 2
+    end
+  end
+end
