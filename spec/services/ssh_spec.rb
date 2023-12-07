f397c15596c30779d794a53096acb92ad179a3c2
Luis Blanco <ljblanco@redborder.com>
2023-12-07 14:05:25 +0000

    refactor node_list call

--- spec/services/ssh_spec.rb
+++ spec/services/ssh_spec.rb
@@ -12,24 +12,15 @@ describe 'Service is enabled and running' do
   end
 end
 
-NODE_LIST_DELEGATE = "red node list 2>/dev/null"
-describe 'Getting node names' do
-  describe command NODE_LIST_DELEGATE do
-    its('exit_status') { should eq 0 }
-    its('stdout') { should_not be_empty }
-  end
-end
-
-nodes = command(NODE_LIST_DELEGATE).stdout.chomp.split("\n")
 describe 'Executing commands into nodes one by one' do
-  it 'At least one node is present' do
-    expect(nodes).not_to be_empty
-  end
-end
+  NODE_LIST = command("red node list 2>/dev/null").stdout.chomp.split("\n")
 
-describe 'Executing commands into nodes one by one' do
-  nodes.each do |node|
-    it "Executing on Node: #{node}" do
+  it 'There is at least one node in the list' do
+    expect(NODE_LIST).not_to be_empty
+  end
+
+  NODE_LIST.each do |node|
+    it "Executing basic echo on Node: #{node}" do
       result = command("red node execute #{node} 'echo SERVERSPEC'")
       expect(result.exit_status).to eq(0)
       expect(result.stdout).to include("#{node}")
@@ -37,3 +28,4 @@ describe 'Executing commands into nodes one by one' do
     end
   end
 end
+
