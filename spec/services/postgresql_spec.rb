eb808bc2ba62b4cf181cf6923e5b2d2c228d70d0
JuanSheba <jsoto@redborder.com>
2023-12-13 14:32:49 +0000

    Same solution to postgresql test

--- spec/services/postgresql_spec.rb
+++ spec/services/postgresql_spec.rb
@@ -6,6 +6,7 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 service = 'postgresql'
 port = 5432
+api_endpoint = 'http://localhost:8500/v1'
 databases = %w[bifrost druid monitors oc_id opscode_chef postgres radius redborder template0 template1]
 
 describe 'Checking PostgreSQL Package...' do
@@ -29,13 +30,16 @@ if service_status == 'enabled'
 
     # Check if PostgreSQL is registered and healthy in Consul
     describe 'Registered in consul' do
-      api_endpoint = 'http://localhost:8500/v1'
-      service_json = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -r '.[]'").stdout
-      health = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'").stdout.strip
-      registered = JSON.parse(service_json).any? && health == 'passing'
-
-      it 'Should be registered and healthy' do
-        expect(registered).to be true
+      service_json_cluster = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -c 'group_by(.ID)[]'")
+      service_json_cluster = service_json_cluster.stdout.chomp.split("\n")
+      health_cluster = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'")
+      health_cluster = health_cluster.stdout.chomp.split("\n")
+      service_and_health = service_json_cluster.zip(health_cluster)
+      service_and_health.each do |service, health|
+        registered = JSON.parse(service)[0].key?('Address') && health == 'passing' # ? true : false
+        it 'Should be registered and enabled' do
+          expect(registered).to be true
+        end
       end
     end
 
