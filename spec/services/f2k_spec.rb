ff1010653ebec27a8d1bb2ab7ca73197aa86790a
JuanSheba <jsoto@redborder.com>
2023-12-13 15:42:38 +0000

    Add solution to test work in cluster and out of it to F2K

--- spec/services/f2k_spec.rb
+++ spec/services/f2k_spec.rb
@@ -7,8 +7,9 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 service = 'f2k'
 port = 2055
 service_status = command("systemctl is-enabled #{service}").stdout.strip
+api_endpoint = 'http://localhost:8500/v1'
 
-packages = %w[f2k]
+packages = %w[cookbook-f2k f2k]
 
 describe "Checking packages for #{service}..." do
   packages.each do |package|
@@ -33,6 +34,20 @@ if service_status == 'enabled'
 
     describe port(port) do
       it { should be_listening }
+    end
+
+    describe 'Registered in consul' do
+      service_json_cluster = command("curl -s #{api_endpoint}/catalog/service/#{service} | jq -c 'group_by(.ID)[]'")
+      service_json_cluster = service_json_cluster.stdout.chomp.split("\n")
+      health_cluster = command("curl -s #{api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'")
+      health_cluster = health_cluster.stdout.chomp.split("\n")
+      service_and_health = service_json_cluster.zip(health_cluster)
+      service_and_health.each do |service, health|
+        registered = JSON.parse(service)[0].key?('Address') && health == 'passing' # ? true : false
+        it 'Should be registered and enabled' do
+          expect(registered).to be true
+        end
+      end
     end
   end
 end
