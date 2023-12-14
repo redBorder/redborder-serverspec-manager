5daaa3c3f125302c72f9a6be11c806548e90e4e1
JuanSheba <jsoto@redborder.com>
2023-12-14 12:12:56 +0000

    Add Serverspec tests for crond service

---
+++ spec/services/crond_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+service = 'crond'
+service_status = command("systemctl is-enabled #{service}").stdout.strip
+packages = %w[cronie]
+
+describe "Checking packages for #{service}..." do
+  packages.each do |package|
+    describe package(package) do
+      before do
+        skip("#{package} is not installed, skipping...") unless package(package).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
+    end
+  end
+end
+
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+end
+
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+  end
+end
+
+# Verificar archivo de configuración si es relevante
+# En sistemas Red Hat, crond generalmente no tiene un archivo de configuración específico, pero puedes agregar verificaciones para archivos crontab si es necesario
