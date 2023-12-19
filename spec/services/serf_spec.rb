592fab7c44abf8622d84d21c0a40ccbfd051fd2b
Vimesa <93142944+ViMesTor@users.noreply.github.com>
2023-12-19 14:30:26 +0000

    Add Serverspec tests for serf service (#64)
    * Add serf spec

---
+++ spec/services/serf_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'json'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+packages = %w[
+  serf redborder-serf
+]
+
+service = 'serf'
+config_file = '/etc/serf/00first.json'
+port = 7946
+
+describe "Checking packages for #{service}..." do
+  packages.each do |package|
+    describe package(package) do
+      before do
+        skip("#{package} is not installed, skipping...") unless package(package).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(package).installed?).to be true
+      end
+    end
+  end
+end
+
+service_status = command("systemctl is-enabled #{service}").stdout
+service_status = service_status.strip
+
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(port) do
+      it { should be_listening }
+    end
+
+    describe file(config_file) do
+      it { should exist }
+      it { should be_file }
+    end
+  end
+end
+
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service}..." do
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+
+    describe file(config_file) do
+      it { should_not exist }
+    end
+  end
+end
