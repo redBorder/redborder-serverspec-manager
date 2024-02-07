fc46e9bab038bb3acdb2e8db54410dff7ffdf098
Luis Blanco <ljblanco@redborder.com>
2024-02-07 10:31:00 +0000

    added rb_check to be in path and executable

--- spec/configuration/cgroup_spec.rb
+++ spec/configuration/cgroup_spec.rb
@@ -34,5 +34,17 @@ describe 'Check cgroups config' do
       it { should be_file }
       its(:content) { should match(/^default\s\d+$/) }
     end
+
+    describe file("/usr/lib/redborder/bin/rb_check_cgroups") do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
+
+    describe file("/usr/lib/redborder/scripts/rb_check_cgroups.rb") do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+    end
   end
 end
