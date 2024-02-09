f038425c416123512fc2ab2b838e45f3745ddab1
nilsver <nverschaeve@redborder.com>
2024-02-09 11:39:27 +0000

    changed variable service to service_name

--- spec/services/rb_arubacentral_spec.rb
+++ spec/services/rb_arubacentral_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-service = 'rb-arubacentral'
-service_status = command("systemctl is-enabled #{service}").stdout.strip
+service_name = 'rb-arubacentral'
+service_status = command("systemctl is-enabled #{service_name}").stdout.strip
 packages = %w[cookbook-rb-arubacentral]
 
-describe "Checking packages for #{service}..." do
+describe "Checking packages for #{service_name}..." do
   packages.each do |p|
     describe package(p) do
       before do
@@ -22,23 +22,23 @@ describe "Checking packages for #{service}..." do
 end
 
 if service_status == 'enabled'
-  describe "Checking #{service_status} service for #{service}..." do
-    describe service(service) do
+  describe "Checking #{service_status} service for #{service_name}..." do
+    describe service(service_name) do
       it { should be_enabled }
       it { should be_running }
     end
   end
 
-  describe "Checking config file for #{service}..." do
-    describe file("/etc/#{service}/config.yml") do
-      it { should exist }
+  describe "Checking config file for #{service_name}..." do
+    describe file("/etc/#{service_name}/config.yml") do
+      it { should exist}
     end
   end
 end
 
 if service_status == 'disabled'
-  describe "Checking #{service_status} service for #{service}..." do
-    describe service(service) do
+  describe "Checking #{service_status} service for #{service_name}..." do
+    describe service(service_name) do
       it { should_not be_enabled }
       it { should_not be_running }
     end
