115f9b7c488059fb891fc017d25ce6193dbdb4bf
nilsver <139124949+nilsver@users.noreply.github.com>
2024-02-15 12:37:47 +0000

    Added arubacentral serverspec test (#67)
    * Added arubacentral serverspec test
    
    * Fix rubocop linter warnings
    
    * Fix trailing whitespaces
    
    * Fix with rubocop -A
    
    * changed name of looping variable
    
    * changed variable service to service_name
    
    * Fix linter errors
    
    * Remove spec configuration duplicate
    
    ---------
    
    Co-authored-by: Miguel Álvarez <128592227+malvads@users.noreply.github.com>
    Co-authored-by: malvads <malvarez@redborder.com>
    Co-authored-by: vimesa <vimesa@redborder.com>

---
+++ spec/services/rb_arubacentral_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+service_name = 'rb-arubacentral'
+service_status = command("systemctl is-enabled #{service_name}").stdout.strip
+packages = %w[cookbook-rb-arubacentral]
+
+describe "Checking packages for #{service_name}..." do
+  packages.each do |p|
+    describe package(p) do
+      before do
+        skip("#{p} is not installed, skipping...") unless package(p).installed?
+      end
+
+      it 'is expected to be installed' do
+        expect(package(p).installed?).to be true
+      end
+    end
+  end
+end
+
+if service_status == 'enabled'
+  describe "Checking #{service_status} service for #{service_name}..." do
+    describe service(service_name) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  describe "Checking config file for #{service_name}..." do
+    describe file("/etc/#{service_name}/config.yml") do
+      it { should exist }
+    end
+  end
+end
+
+if service_status == 'disabled'
+  describe "Checking #{service_status} service for #{service_name}..." do
+    describe service(service_name) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+  end
+end
