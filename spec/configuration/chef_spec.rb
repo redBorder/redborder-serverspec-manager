e0a66f58ae7190e9d14259e77c2716b9c86f4bfd
Luis Blanco <ljblanco@redborder.com>
2024-04-16 15:49:16 +0100

    adding test to see executable exists

---
+++ spec/configuration/chef_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+describe file('/usr/lib/redborder/bin/rb_wakeup_chef.sh'), :rb_wakeup_chef do
+  it { should exist }
+  it { should be_file }
+  it { should be_executable }
+end
