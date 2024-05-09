e98ecc56923c75eafa818dbc64e46844ba20da89
Luis Blanco <ljblanco@redborder.com>
2024-05-09 11:14:45 +0100

    check config files exist in current location

---
+++ spec/configuration/ale_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+describe 'Check ale config' do
+  describe file('/etc/redborder-ale/schema.pb.rb') do
+    it { should exist }
+    it { should be_file }
+  end
+
+  describe file('/etc/redborder-ale/config.yml') do
+    it { should exist }
+    it { should be_file }
+  end
+
+  describe file('/etc/redborder-ale/rb_ale_aps.conf') do
+    it { should exist }
+    it { should be_file }
+  end
+end
+
+describe file('/var/rb-ale/bin/rb_ale.rb') do
+  it { should exist }
+  it { should be_file }
+  it { should be_executable.by(:owner) }
+  it { should be_executable.by(:group) }
+  its(:content) { should match(%r{^(\s*#.*|)#!/usr/bin/env\s+ruby.*$}) }
+end
