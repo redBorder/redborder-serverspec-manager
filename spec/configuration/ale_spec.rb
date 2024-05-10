596e95fa9e313be15b7f253d47a44983ac06d744
Luis Blanco <ljblanco@redborder.com>
2024-05-10 10:46:31 +0100

    check the user has selected by default the corresponding rbuy version and gemset

--- spec/configuration/ale_spec.rb
+++ spec/configuration/ale_spec.rb
@@ -27,3 +27,14 @@ describe file('/var/rb-ale/bin/rb_ale.rb') do
   it { should be_executable.by(:group) }
   its(:content) { should match(%r{^(\s*#.*|)#!/usr/bin/env\s+ruby.*$}) }
 end
+
+describe 'Redborder-Ale user config' do
+  describe command('sudo -u redborder-ale bash -lc "ruby -v"') do
+    its(:stdout) { should match /ruby 2.7.5/ }
+  end
+
+# Notice this is a bash session, when closed the gemset will change to default
+  describe command('sudo -u redborder-ale bash -lc "rvm gemset list"') do
+    its(:stdout) { should match /\=\>\s*global/ }
+  end
+end
