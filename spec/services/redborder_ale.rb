5fdc80383a9906d5067e7ad6b14d35df2b5f7f2b
Luis Blanco <ljblanco@redborder.com>
2024-05-15 10:27:28 +0100

    Split packages that are always installed vs depending cases. Depending cases should be if the service is up and running

--- spec/services/redborder_ale.rb
+++ spec/services/redborder_ale.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'set'
+
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
+SERVICE = 'redborder-ale'
+MUST_INSTALLED_PKGS = Set.new(%w[cookbook-rb-ale])
 
-service = 'redborder-ale'
-service_status = command("systemctl is-enabled #{service}").stdout.strip
-packages = %w[redborder-ale]
-
-describe "Checking packages for #{service}..." do
-  packages.each do |package|
+describe "Checking packages for #{SERVICE}..." do
+  MUST_INSTALLED_PKGS.each do |package|
     describe package(package) do
-      before do
-        skip("#{package} is not installed, skipping...") unless package(package).installed?
-      end
-
       it 'is expected to be installed' do
         expect(package(package).installed?).to be true
       end
@@ -21,11 +17,18 @@ describe "Checking packages for #{service}..." do
   end
 end
 
-describe "Checking #{service_status} service for #{service}..." do
-  describe service(service) do
+INSTALLED_WHEN_NEEDED_PKGS = Set.new(%w[redborder-ale])
+are_pkgs_installed = MUST_INSTALLED_PKGS.merge(INSTALLED_WHEN_NEEDED_PKGS).all? do |pkg|
+  package(pkg).installed?
+end
+
+service_status = command("systemctl is-enabled #{SERVICE}").stdout.strip
+describe "Checking #{service_status} service for #{SERVICE}..." do
+  describe service(SERVICE) do
     if service_status == 'enabled'
       it { should be_enabled }
       it { should be_running }
+      expect(are_pkgs_installed).to be true
     elsif service_status == 'disabled'
       it { should_not be_enabled }
       it { should_not be_running }
@@ -34,6 +37,6 @@ describe "Checking #{service_status} service for #{service}..." do
 end
 
 command_to_check = '/usr/lib/rvm/rubies/ruby-2.7.5/bin/ruby'
-describe command("sudo -u #{service} #{command_to_check} -e 'exit 0' 2>&1") do
+describe command("sudo -u #{SERVICE} #{command_to_check} -e 'exit 0' 2>&1") do
   its(:exit_status) { should eq 0 }
 end
