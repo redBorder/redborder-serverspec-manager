cebd3ac0f7e3e47e4b2d744b27b6e4e6fd9c6a51
Luis Blanco <ljblanco@redborder.com>
2024-05-16 16:41:24 +0100

    check rb_wakeup_chef.sh is executable by the webui user

--- spec/configuration/chef_spec.rb
+++ spec/configuration/chef_spec.rb
@@ -6,5 +6,5 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 describe file('/usr/lib/redborder/bin/rb_wakeup_chef.sh'), :rb_wakeup_chef do
   it { should exist }
   it { should be_file }
-  it { should be_executable }
+  it { should be_executable.by_user('webui') }
 end
