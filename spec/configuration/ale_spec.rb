6f05cd49af618fb8c8941b8eb0eee61e2761a79b
Luis Blanco <ljblanco@redborder.com>
2024-06-10 11:00:17 +0100

    just checking the package is installed. Service was going to be stopped because files would not exist

--- spec/configuration/ale_spec.rb
+++ spec/configuration/ale_spec.rb
@@ -3,35 +3,38 @@
 require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-# TODO: only do it if ale  is enabled
-describe 'Check ale config' do
-  describe file('/etc/redborder-ale/schema.pb.rb') do
-    it { should exist }
-    it { should be_file }
+is_package_installed = package('redborder-ale').installed?
+
+if is_package_installed
+  describe 'Check ale config' do
+    describe file('/etc/redborder-ale/schema.pb.rb') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe file('/etc/redborder-ale/config.yml') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe file('/etc/redborder-ale/rb_ale_aps.conf') do
+      it { should exist }
+      it { should be_file }
+    end
   end
 
-  describe file('/etc/redborder-ale/config.yml') do
+  describe file('/var/rb-ale/bin/rb_ale.rb') do
     it { should exist }
     it { should be_file }
+    it { should be_executable.by(:owner) }
+    it { should be_executable.by(:group) }
+    its(:content) { should match(%r{^(\s*#.*|)#!/usr/bin/env\s+ruby.*$}) }
   end
 
-  describe file('/etc/redborder-ale/rb_ale_aps.conf') do
+  describe file('/usr/bin/rb_scan_ale.rb') do
     it { should exist }
     it { should be_file }
+    it { should be_executable.by(:owner) }
+    it { should be_executable.by(:group) }
   end
-end
-
-describe file('/var/rb-ale/bin/rb_ale.rb') do
-  it { should exist }
-  it { should be_file }
-  it { should be_executable.by(:owner) }
-  it { should be_executable.by(:group) }
-  its(:content) { should match(%r{^(\s*#.*|)#!/usr/bin/env\s+ruby.*$}) }
-end
-
-describe file('/usr/bin/rb_scan_ale.rb') do
-  it { should exist }
-  it { should be_file }
-  it { should be_executable.by(:owner) }
-  it { should be_executable.by(:group) }
 end
