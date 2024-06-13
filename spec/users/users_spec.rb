216b5ce0ebb917f87456ccc078390ce79840bc20
Luis Blanco <ljblanco@redborder.com>
2024-06-13 16:33:52 +0100

    optimization

--- spec/users/users_spec.rb
+++ spec/users/users_spec.rb
@@ -7,16 +7,14 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 describe 'Checking only these users has login permission' do
   passwd = command('cat /etc/passwd').stdout.split("\n")
-  all_users = passwd.map { |p| p.split(':').first }
-  all_users = Set.new all_users
+  bash_users = passwd.select { |p| p.include? '/bin/bash' }
+  bash_users.map! { |p| p.split(':').first }
+  bash_users = Set.new bash_users
 
-  allowed_users = Set.new %w[root redborder postgres]
-  not_allowed_users = all_users - allowed_users
+  allowed_users = Set.new %w[root redborder]
+  not_allowed_users = bash_users - allowed_users
 
-  not_allowed_users.each do |user|
-    describe user(user) do
-      it { should exist }
-      it { should_not have_login_shell('/bin/bash') }
-    end
+  it 'should only allow specified users to have login permissions' do
+    expect(not_allowed_users).to be_empty
   end
 end
