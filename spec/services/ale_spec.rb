988b6e47aa71f8cc37e64ab0b4fcef354845de9e
Luis Blanco <ljblanco@redborder.com>
2024-06-17 09:23:57 +0100

    conditional to every configuration and service is conditional also and has no port

--- spec/services/ale_spec.rb
+++ spec/services/ale_spec.rb
@@ -5,39 +5,22 @@ require 'set'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 service = 'redborder-ale'
-package = 'redborder-ale'
 
-describe "Checking packages for #{service}..." do
-    describe package(package) do
-      before do
-        skip("#{package} is not installed, skipping...") unless package(package).installed?
-      end
+is_package_installed = package('redborder-ale').installed?
 
-      it 'is expected to be installed' do
-        expect(package(package).installed?).to be true
-      end
-    end
-end
-
-service_status = command("systemctl is-enabled #{service}").stdout.strip
-describe "Checking #{service_status} service for #{service}..." do
-  describe service(service) do
-    if service_status == 'enabled'
-      it { should be_enabled }
-      it { should be_running }
-    end
-  end
-end
-
-if service_status == 'disabled'
+if is_package_installed
+  service_status = command("systemctl is-enabled #{service}").stdout.strip
   describe "Checking #{service_status} service for #{service}..." do
-    describe service(service) do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
-
-    describe port(port) do
-      it { should_not be_listening }
+    if service_status == 'enabled'
+      describe service(service) do
+        it { should be_enabled }
+        it { should be_running }
+      end
+    elsif service_status == 'disabled'
+      describe service(service) do
+        it { should_not be_enabled }
+        it { should_not be_running }
+      end
     end
   end
 end
