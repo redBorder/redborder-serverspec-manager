0422e4651fb8416cfa71b4a3843dc4dec3233752
Luis Blanco <ljblanco@redborder.com>
2024-06-17 15:13:21 +0100

    the path is a little different for no login here

--- spec/users/ssh_spec.rb
+++ spec/users/ssh_spec.rb
@@ -5,5 +5,5 @@ set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 describe user('sshd') do
   it { should exist }
-  it { should have_login_shell('/sbin/nologin') }
+  it { should have_login_shell('/usr/sbin/nologin') }
 end
