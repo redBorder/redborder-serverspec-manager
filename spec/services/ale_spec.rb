98f77b9747844191ec8cbea07c3e21188bc69fe3
Luis Blanco <ljblanco@redborder.com>
2024-06-25 16:06:25 +0100

    simplify ale pkg and service check. Removed ruby path checking because not working

--- spec/services/ale_spec.rb
+++ spec/services/ale_spec.rb
@@ -4,45 +4,35 @@ require 'spec_helper'
 require 'set'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-service = 'redborder-ale'
+service = pkg = 'redborder-ale'
 
 describe "Checking packages for #{service}..." do
-  describe package(package) do
+  describe package(pkg) do
     before do
-      skip("#{package} is not installed, skipping...") unless package(package).installed?
+      skip("#{pkg} is not installed, skipping...") unless package(pkg).installed?
     end
 
     it 'is expected to be installed' do
-      expect(package(package).installed?).to be true
+      expect(package(pkg).installed?).to be true
     end
   end
 end
 
 service_status = command("systemctl is-enabled #{service}").stdout.strip
 describe "Checking #{service_status} service for #{service}..." do
-  describe service(service) do
-    if service_status == 'enabled'
+  if service_status == 'enabled'
+    describe service(service) do
       it { should be_enabled }
       it { should be_running }
     end
-  end
-end
-
-describe 'Redborder-ale is using correct ruby setup' do
-  if service_status == 'enabled'
-    describe command('sudo -u redborder-ale which ruby') do
-      its(:stdout) { should match %r{/usr/lib/rvm/rubies/ruby-2.7.5/bin/ruby} }
-    end
   elsif service_status == 'disabled'
-    describe "Checking #{service_status} service for #{service}..." do
-      describe service(service) do
-        it { should_not be_enabled }
-        it { should_not be_running }
-      end
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
 
-      describe port(port) do
-        it { should_not be_listening }
-      end
+    describe port(port) do
+      it { should_not be_listening }
     end
   end
 end
