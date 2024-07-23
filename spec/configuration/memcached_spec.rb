179218485ad0d62a28ff5fb2f987c24e04ed7aca
Luis Blanco <ljblanco@redborder.com>
2024-07-23 10:14:23 +0100

    checking config file doesn't contain localhost. Only node names are allowed.

---
+++ spec/configuration/memcached_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'json'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+describe 'Memcached config' do
+  describe file('/var/www/rb-rails/config/memcached_config.yml') do
+    it { should exist }
+    it { should be_file }
+    it { should_not contain 'localhost' }
+  end
+end
