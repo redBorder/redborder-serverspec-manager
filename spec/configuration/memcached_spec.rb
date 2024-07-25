c3fbf151c8be9c1c3e6096085fc8c6c81965be00
Luis Blanco <108473576+ljblancoredborder@users.noreply.github.com>
2024-07-25 13:50:27 +0100

    Update memcached_spec.rb

--- spec/configuration/memcached_spec.rb
+++ spec/configuration/memcached_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'json'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-describe 'Memcached config' do
+describe 'Memcached config in webui' do
   describe file('/var/www/rb-rails/config/memcached_config.yml') do
     it { should exist }
     it { should be_file }
