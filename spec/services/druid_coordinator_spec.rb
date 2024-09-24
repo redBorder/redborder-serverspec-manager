c407d75bb227c25a245d488ba20682f7bb5e9616
Luis Blanco <ljblanco@redborder.com>
2024-09-24 17:53:18 +0100

    LINT

--- spec/services/druid_coordinator_spec.rb
+++ spec/services/druid_coordinator_spec.rb
@@ -27,7 +27,8 @@ describe "Checking #{packages}" do
   end
 
   describe 'Registered in consul' do
-    service_json_cluster = command("curl -s #{consul_api_endpoint}/catalog/service/#{service} | jq -c 'group_by(.ID)[]'")
+    get_service_json_cmd = "curl -s #{consul_api_endpoint}/catalog/service/#{service} | jq -c 'group_by(.ID)[]'"
+    service_json_cluster = command(get_service_json_cmd)
     service_json_cluster = service_json_cluster.stdout.chomp.split("\n")
     health_cluster = command("curl -s #{consul_api_endpoint}/health/service/#{service} | jq -r '.[].Checks[0].Status'")
     health_cluster = health_cluster.stdout.chomp.split("\n")
@@ -44,7 +45,9 @@ describe "Checking #{packages}" do
     # Sample of wrong
     # {"_default":[{"tieredReplicants":{"_default_tier":2},"type":"loadForever"}]}
     # Sample of expected
-    # {"_default":[{"period":"P1M","tieredReplicants":{"_default_tier":1},"type":"loadByPeriod"},{"type":"dropForever"}]}
+    # {"_default":[
+    #     {"period":"P1M","tieredReplicants":{"_default_tier":1},"type":"loadByPeriod"},{"type":"dropForever"}
+    #     ]}
 
     get_default_rules_cmd = "curl -X GET http://#{service}.service:8081/druid/coordinator/v1/rules/"
     describe command(get_default_rules_cmd) do
