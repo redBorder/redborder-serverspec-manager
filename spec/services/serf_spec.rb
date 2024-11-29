d533ed15c2e94705f6b5096bb2f36bf441f94210
Luis Blanco <ljblanco@redborder.com>
2024-11-29 15:37:47 +0000

    add port 7373. Not sure if port 5353 is really necessary

--- spec/services/serf_spec.rb
+++ spec/services/serf_spec.rb
@@ -10,9 +10,7 @@ packages = %w[
 
 service = 'serf'
 config_file = '/etc/serf/00first.json'
-port = 7946
-# port_mdns = 5353 # test is listening next time I build a cluster
-
+ports = [7946, 7373]
 describe "Checking packages for #{service}..." do
   packages.each do |package|
     describe package(package) do
@@ -37,13 +35,11 @@ if service_status == 'enabled'
       it { should be_running }
     end
 
-    describe port(port) do
-      it { should be_listening }
+    ports.each do |p|
+      describe port(p) do
+        it { should be_listening }
+      end
     end
-
-    # describe port(port_mdns) do
-    #   it { should be_listening }
-    # end
 
     describe file(config_file) do
       it { should exist }
