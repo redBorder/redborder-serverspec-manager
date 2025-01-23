5b10c4f99905ebfa5fe684b826780233ac9d79d8
Luis Blanco <ljblanco@redborder.com>
2025-01-23 10:18:22 +0000

    sort status by the condition of setup and normal funtioning of the manager

--- spec/services/rb_bootstrap_spec.rb
+++ spec/services/rb_bootstrap_spec.rb
@@ -4,19 +4,19 @@ require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
 service = 'rb-bootstrap'
-service_status = command("systemctl is-enabled #{service}").stdout.strip
-
 describe "Checking service status for #{service}..." do
   describe service(service) do
-    if service_status == 'enabled'
-      it { should be_enabled }
-    else
+    active_state = command("systemctl show -p ActiveState --value #{service}").stdout.strip
+    active_status = command("systemctl is-active #{service}").exit_status
+    if active_state == 'inactive'
       it { should_not be_enabled }
-    end
-
-    it "is expected to be in a state of 'active (exited)'" do
-      expect(command("systemctl show -p ActiveState --value #{service}").stdout.strip).to eq 'active'
-      expect(command("systemctl show -p SubState --value #{service}").stdout.strip).to eq 'exited'
+      it { expect(active_status).to eq(3) } # 3 is not running
+      failed_status = command("systemctl is-failed #{service}").exit_status
+      it { expect(failed_status).to eq(1) } # 1 has finished with success
+    elsif active_state == 'active'
+    # On setup the service has to be active
+      # it { should be_enabled }
+      it { expect(active_status).to eq(0) } # 0 is running
     end
   end
 end
